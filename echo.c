44be1a43451605c979d84155d9474126da98d424
Mason Jennings <masonj@csu.fullerton.edu>
2025-05-09 21:38:10 -0700

    added imports

--- echo.c
+++ echo.c
@@ -1,4 +1,11 @@
+// #include <arpa/inet.h>
+// #include <netinet/in.h>
+// #include <pthread.h>
 #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <sys/socket.h>
+// #include <unistd.h>
 
 int main() {
     printf("Hello world!\n");
